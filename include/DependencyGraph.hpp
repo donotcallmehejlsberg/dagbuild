a4228b1eb0c6062d8e1b8a0be9e358af7c0e924f
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-08-08 17:00:21 +0200

    feat: implement recursive dependency traversal

--- include/DependencyGraph.hpp
+++ include/DependencyGraph.hpp
@@ -6,9 +6,15 @@
 
 #include "BuildTarget.hpp"
 
+enum class VisitState { NotVisited, Visiting, Visited };
+
 class DependencyGraph {
  private:
   const std::unordered_map<std::string, BuildTarget> &targets_;
+
+  bool dfs(const std::string &currentTargetName,
+           std::unordered_map<std::string, VisitState> &states,
+           std::vector<std::string> &buildOrder);
 
  public:
   DependencyGraph(const std::unordered_map<std::string, BuildTarget> &targets);
--- src/DependencyGraph.cpp
+++ src/DependencyGraph.cpp
@@ -1,7 +1,41 @@
 #include "DependencyGraph.hpp"
 
+#include <iostream>
+
 DependencyGraph::DependencyGraph(
     const std::unordered_map<std::string, BuildTarget> &targets)
     : targets_(targets) {}
 
-    
+bool DependencyGraph::dfs(const std::string &currentTargetName,
+                          std::unordered_map<std::string, VisitState> &states,
+                          std::vector<std::string> &buildOrder) {
+  const VisitState currentState = states.at(currentTargetName);
+  if (currentState == VisitState::Visiting) {
+    std::cerr << "Error: dependency cycle detected at target '"
+              << currentTargetName << "'.\n";
+    return false;
+  }
+
+  if (currentState == VisitState::Visited) {
+    return true;
+  }
+
+  states.at(currentTargetName) = VisitState::Visiting;
+  const auto targetIterator = targets_.find(currentTargetName);
+  if (targetIterator == targets_.end()) {
+    std::cerr << "Error: target '" << currentTargetName << "' not found.\n";
+    return false;
+  }
+
+  const BuildTarget &currentTarget = targetIterator->second;
+  for (auto &dependencyName : currentTarget.dependencyNames) {
+    if (!dfs(dependencyName, states, buildOrder)) {
+      return false;
+    }
+  }
+
+  states.at(currentTargetName) = VisitState::Visited;
+  buildOrder.push_back(currentTargetName);
+
+  return true;
+}
