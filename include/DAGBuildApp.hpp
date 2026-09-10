4b3c063dc77ace982b212558aefa47ec037090e9
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-09-10 20:00:46 +0200

    refactor: move application logic from main to DAGBuildApp

---
+++ include/DAGBuildApp.hpp
@@ -0,0 +1,10 @@
+#ifndef DAG_BUILD_APP
+#define DAG_BUILD_APP
+
+class DAGBuildApp
+{
+public:
+  int run(int argc, char *argv[]);
+};
+
+#endif // DAG_BUILD_APP
---
+++ src/DAGBuildApp.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+#include "Builder.hpp"
+#include "CommandRunner.hpp"
+#include "ConfigParser.hpp"
+#include "ExitCodes.hpp"
+#include "DAGBuildApp.hpp"
+
+namespace {
+
+constexpr char CONFIG_PATH[] = "dagbuild.conf";
+
+}  // namespace
+
+int DAGBuildApp::run(int argc, char *argv[]) {
+  Builder builder;
+  ConfigParser configParser;
+  CommandRunner commandRunner;
+
+  if (argc < 2) {
+    std::cerr << "Error: no command provided.\n";
+    std::cerr << "Use '" << argv[0] << " help' for usage information.\n";
+    return ExitCode::INVALID_COMMAND;
+  }
+
+  const std::string command = argv[1];
+  if (command == "help") {
+    commandRunner.runPrintHelp(argv[0]);
+    return ExitCode::SUCCESS;
+  }
+
+  if (command == "build") {
+    if (argc != 3 && argc != 5 && argc != 7) {
+      std::cerr << "Usage: " << argv[0]
+                << " build <target> [--jobs <number>] "
+                   "[--mode <debug|release>]\n";
+      return ExitCode::INVALID_COMMAND;
+    }
+    return commandRunner.runBuildCommand(builder, configParser, CONFIG_PATH,
+                                         argc, argv);
+  }
+
+  if (command == "clean") {
+    if (argc != 2) {
+      std::cerr << "Error: clean does not accept additional arguments.\n";
+      return ExitCode::INVALID_COMMAND;
+    }
+    return commandRunner.runClean(builder);
+  }
+
+  if (command == "list") {
+    if (argc != 2) {
+      std::cerr << "Error: list does not accept additional arguments.\n";
+      return ExitCode::INVALID_COMMAND;
+    }
+    const auto targets = configParser.parseTargets(CONFIG_PATH);
+    return commandRunner.runListCommand(targets);
+  }
+
+  std::cerr << "Error: unknown command '" << command << "'.\n";
+  std::cerr << "Use '" << argv[0] << " help' for usage information.\n";
+  return ExitCode::INVALID_COMMAND;
+}
